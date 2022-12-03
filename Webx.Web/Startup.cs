0d850211c207f551ea06efff6b66ad0e66b65b9f
telmorvf <87204667+telmorvf@users.noreply.github.com>
2022-12-03 14:42:56 +0000

    Rivisões acrodas na reunião, gestão de imagens de produto

--- Webx.Web/Startup.cs
+++ Webx.Web/Startup.cs
@@ -136,7 +136,7 @@ namespace Webx.Web
 
             });
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
 
