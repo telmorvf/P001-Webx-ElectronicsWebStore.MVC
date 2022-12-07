016e0d1496aa61ea272dcbbdaf5e4b5ed8c74b96
FFerreira92 <F92ferreira@gmail.com>
2022-12-07 10:28:58 +0000

    Changed layoutFooter

--- Webx.Web/Controllers/HomeController.cs
+++ Webx.Web/Controllers/HomeController.cs
@@ -86,14 +86,37 @@ namespace Webx.Web.Controllers
             return View(model);
         }
 
-        public IActionResult AboutUs()
+        public async Task<IActionResult> AboutUs()
         {
-            return View();
+
+            var model = await _productRepository.GetInitialShopViewModelAsync();
+            model.WishList = await _productRepository.GetOrStartWishListAsync();
+            model.Brands = (List<Brand>)await _brandRepositoty.GetAllBrandsAsync();
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = await _userHelper.GetUserByEmailAsync(User.Identity.Name);
+                ViewBag.UserFullName = user.FullName;
+                ViewBag.IsActive = user.Active;
+            }
+
+
+            return View(model);
         }
 
-        public IActionResult ContactUs()
+        public async Task<IActionResult> ContactUs()
         {
-            return View();
+            var model = await _productRepository.GetInitialShopViewModelAsync();
+            model.WishList = await _productRepository.GetOrStartWishListAsync();
+            model.Brands = (List<Brand>)await _brandRepositoty.GetAllBrandsAsync();
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = await _userHelper.GetUserByEmailAsync(User.Identity.Name);
+                ViewBag.UserFullName = user.FullName;
+                ViewBag.IsActive = user.Active;
+            }
+
+
+            return View(model);
         }
 
 
